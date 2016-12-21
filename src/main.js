4eac4d06ce22c0991c5da7f5025515950f920246
dfontana <dyfontana@gmail.com>
2016-12-21 15:51:26 -0500

    Data over Async handled properly.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 var todoistAPI = require('./todoistAPI');
 var api = new todoistAPI();
+var vorpal = require('vorpal')();
 
-api.printdata();
+var alldata = {};
+
+vorpal
+    .delimiter('Task:')
+    .show();
+
+vorpal
+    .command('clear')
+    .action(function(args, cb) {
+        process.stdout.write("\u001B[2J\u001B[0;0f");
+        cb();
+    });
+
+vorpal
+    .command('list', 'Lists projects')
+    .action(function(args, cb) {
+        var callback = function(projects) {
+            console.log(projects);
+        };
+        api.projects(callback, cb);
+    });
--- src/todoistAPI.js
+++ src/todoistAPI.js
@@ -1,28 +1,34 @@
-function todoistAPI(){
+function todoistAPI() {
     var request = require('request');
-    var user    = require('../.task.json');
+    var user = require('../.task.json');
 
-    var entry   = 'https://todoist.com/API/v7/sync';
-
-    this.printdata = function() {
-        var alldata = {
-            url: entry, 
+    var entry = 'https://todoist.com/API/v7/sync';
+    var sync_token = '*';
+    var alldata = {};
+   
+    this.projects = function(dataProcessor, cb) {
+        var payload = {
+            url: entry,
             form: {
                 token: user.token,
-                sync_token: '*',
-                resource_types: '["all"]'
+                sync_token: sync_token,
+                resource_types: '["projects"]'
             }
-        };  
+        };   
 
-        request.post(alldata, function(err, httpResponse, body){
-                if(err){
-                    console.log(err);
-                }else{
-                    console.log('\n\n===== HTTP ====\n'+httpResponse);
-                    console.log('\n\n===== BODY ====\n'+body);
-                }
+        //TODO implement the sync difference API
+        request.post(payload, function(err, httpResponse, body) {
+            if (!err && httpResponse.statusCode == 200) {
+                var parsed = JSON.parse(body);
+                //sync_token = parsed.sync_token;
+                alldata.projects = parsed.projects;
+                dataProcessor(alldata.projects);
+                cb();
+            } else {
+                console.log("[projects] ERR: " + httpResponse.statusCode + "\n\t" + err + "\n");
+                cb();
             }
-        );
+        });
     };
 }
 
