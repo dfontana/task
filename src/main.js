0d2b15442e19e750a77327d12b25ac86acf361fc
dfontana <dyfontana@gmail.com>
2016-12-21 23:20:26 -0500

    Added coloring to list

--- src/main.js
+++ src/main.js
@@ -5,6 +5,31 @@ var inq = require('inquirer');
 
 var alldata = {};
 
+var colors = {
+    0: vorpal.chalk.green,
+    1: vorpal.chalk.red,
+    2: vorpal.chalk.red,
+    3: vorpal.chalk.yellow,
+    4: vorpal.chalk.blue,
+    5: vorpal.chalk.white,
+    6: vorpal.chalk.magenta,
+    7: vorpal.chalk.gray,
+    8: vorpal.chalk.red,
+    9: vorpal.chalk.yellow,
+    10: vorpal.chalk.blue,
+    11: vorpal.chalk.blue,
+    12: vorpal.chalk.magenta,
+    13: vorpal.chalk.red,
+    14: vorpal.chalk.red,
+    15: vorpal.chalk.green,
+    16: vorpal.chalk.cyan,
+    17: vorpal.chalk.cyan,
+    18: vorpal.chalk.cyan,
+    19: vorpal.chalk.cyan,
+    20: vorpal.chalk.black,
+    21: vorpal.chalk.gray
+};
+
 function startWaitingIndicator() {
     var frames = [' ', ' ', '…', '……', '………', '…………', '……………'];
     var i = 0;
@@ -33,10 +58,12 @@ function projectSelection(volself, cb) {
                 name += ' ';
             }
             name += projects[key].name;
-            obj.name = vorpal.chalk.green(name);
+            obj.name = colors[projects[key].color](name);
             obj.value = projects[key];
             obj.short = name;
             projlist.push(obj);
+
+            volself.log(obj.value);
         }
 
         projlist.push(new inq.Separator());
