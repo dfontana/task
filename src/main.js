51df60f6799a9747dc9561b698dc4a26306c7c54
dfontana <dyfontana@gmail.com>
2016-12-21 20:49:35 -0500

    Prompt fixed and working. Implementing task list.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,99 @@
 var todoistAPI = require('./todoistAPI');
 var api = new todoistAPI();
 var vorpal = require('vorpal')();
+var inq = require('inquirer');
 
 var alldata = {};
+
+function startWaitingIndicator() {
+    var frames = [' ', ' ', '…', '……', '………', '…………', '……………'];
+    var i = 0;
+    var intervalId = setInterval(function() {
+        var frame = frames[i = ++i % frames.length];
+        vorpal.ui.redraw('Hold on …' + frame);
+    }, 250);
+    return intervalId;
+}
+
+function stopWaitingIndicator(intervalId) {
+    clearInterval(intervalId);
+    vorpal.ui.redraw.done();
+}
+
+function projectSelection(volself, cb) {
+    var listProjectsIndicator = startWaitingIndicator();
+    api.projects(function(projects) {
+        stopWaitingIndicator(listProjectsIndicator);
+
+        var projlist = [];
+        for (var key in projects) {
+            var obj = {};
+            var name = '';
+            for(var i = 1; i < projects[key].indent; i++){
+                name += ' ';
+            }
+            name += projects[key].name;
+            obj.name = name;
+            obj.value = projects[key];
+            obj.short = name;
+            projlist.push(obj);
+        }
+
+        projlist.push(new inq.Separator());
+        projlist.push('.. Cancel');
+        projlist.push(new inq.Separator());
+
+
+        volself.prompt({
+            type: 'list',
+            name: 'project',
+            message: 'Select a project to view tasks',
+            choices: projlist
+        }, function(result) {
+            var selectedProject = result.project;
+
+            volself.log(selectedProject);
+
+            if (selectedProject == '.. Cancel') {
+                cb();
+            } else {
+                taskSelection(volself, selectedProject.id, cb);
+            }
+        });
+    });
+}
+
+function taskSelection(volself, projid, cb) {
+    var listTaskIndicator = startWaitingIndicator();
+    api.tasks(projid, function(tasks) {
+        stopWaitingIndicator(listTaskIndicator);
+
+        var tasklist = [];
+        for (var key in tasks) {
+            tasklist.push(key);
+        }
+        tasklist.push('.. Return to Project List');
+        tasklist.push('.. Cancel');
+
+        volself.prompt({
+            type: 'rawlist',
+            name: 'task',
+            message: 'Select a task to perform an action',
+            choices: tasklist
+        }, function(result) {
+            if (result.task == '.. Return to Project List') {
+                projectSelection();
+            } else if (result.task == '.. Cancel') {
+                cb();
+            } else {
+                //TODO act on selected task
+                volself.log('Hit endpoint\n');
+                cb();
+            }
+        });
+
+    });
+}
 
 vorpal
     .delimiter('Task:')
@@ -18,8 +109,6 @@ vorpal
 vorpal
     .command('list', 'Lists projects')
     .action(function(args, cb) {
-        var callback = function(projects) {
-            console.log(projects);
-        };
-        api.projects(callback, cb);
+        var volself = this;
+        projectSelection(volself, cb);
     });
--- src/todoistAPI.js
+++ src/todoistAPI.js
@@ -5,8 +5,12 @@ function todoistAPI() {
     var entry = 'https://todoist.com/API/v7/sync';
     var sync_token = '*';
     var alldata = {};
-   
-    this.projects = function(dataProcessor, cb) {
+
+    /**
+     * Obtains project list from Todoist, afterwhich passing it to the given dataProcessor callback.
+     * @param dataProcessor - callback that handles the finalized data set
+     */
+    this.projects = function(dataProcessor) {
         var payload = {
             url: entry,
             form: {
@@ -14,7 +18,7 @@ function todoistAPI() {
                 sync_token: sync_token,
                 resource_types: '["projects"]'
             }
-        };   
+        };
 
         //TODO implement the sync difference API
         request.post(payload, function(err, httpResponse, body) {
@@ -23,12 +27,24 @@ function todoistAPI() {
                 //sync_token = parsed.sync_token;
                 alldata.projects = parsed.projects;
                 dataProcessor(alldata.projects);
-                cb();
             } else {
                 console.log("[projects] ERR: " + httpResponse.statusCode + "\n\t" + err + "\n");
-                cb();
             }
         });
+    };
+
+    /**
+     * Returns all tasks, or optionally if a project id is given, the tasks for that project.
+     * @param projid - id of the project [optional]
+     * @param dataProcessor - callback that processes the final task data set 
+     */
+    this.tasks = function(projid, dataProcessor) {
+        if(projid){
+            //id
+            
+        }else{
+            //all tasks
+        }
     };
 }
 
